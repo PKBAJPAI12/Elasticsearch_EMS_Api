2c32c3c6f1073d21a094c933b1e582c3a7b3b9b6
unknown <prateek.cse.uiet@gmail.com>
2024-10-07 00:01:01 +0530

    add api route for addEmployee in elasticsearch

--- myproject/employees/urls.py
+++ myproject/employees/urls.py
@@ -1,7 +1,8 @@
 # employees/urls.py
 from django.urls import path
-from .views import get_employees
+from .views import get_employees, create_employee
 
 urlpatterns = [
     path('getEmployees/', get_employees),
+    path('addEmployees/', create_employee),
 ]
--- myproject/employees/views.py
+++ myproject/employees/views.py
@@ -3,25 +3,93 @@ from django.shortcuts import render
 # Create your views here.
 # employees/views.py
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from .es_connection import es
+import json
 
 def get_employees(request):
     query_value = request.GET.get('query', '')
+
+    if query_value:
+        response = es.search(index="employee_db", body={
+            "query": {
+                "match": {
+                    "FirstName": {
+                        "query": query_value,
+                        "fuzziness": "AUTO"
+                    }
+                }
+            }
+        })
+    else:
+        # If no query is provided, return all employees
+        response = es.search(index="employee_db", body={
+            "size": 10000,
+            "query": {
+                "match_all": {}
+            }
+        })
     
-    resp = es.search(index="employee_db", body={
-        "size": 1000,
-        "query": {
-            "match": {
-                "FirstName": {
-                    "query": query_value,
-                    "fuzziness": "AUTO"
+    return JsonResponse(response['hits']['hits'], safe=False)
+
+@csrf_exempt
+def create_employee(request):
+    try:
+        if request.method != 'POST':
+            return JsonResponse({"error": "POST request required."}, status=405)
+
+        data = json.loads(request.body)
+
+        if not data.get("first_name") or not data.get("last_name"):
+            return JsonResponse({"error": "First Name and Last Name are required."}, status=400)
+        
+        existing_employee_query = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {"match": {"FirstName": data.get("first_name")}},
+                        {"match": {"LastName": data.get("last_name")}},
+                        {"match": {"Designation": data.get("designation")}},
+                        {"match": {"Salary": data.get("salary")}},
+                        {"match": {"DateOfJoining": data.get("date_of_joining")}},
+                        {"match": {"Address": data.get("address")}},
+                        {"match": {"Gender": data.get("gender")}},
+                        {"match": {"Age": data.get("age")}},
+                        {"match": {"MaritalStatus": data.get("marital_status")}},
+                        {"match": {"Interests": data.get("interests")}}
+                    ]
                 }
             }
         }
-    })
-    
-    resp_body = resp.body if hasattr(resp, 'body') else resp
+        existresponse = es.search(index="employee_db", body=existing_employee_query)
+        print(existresponse['hits']['total']['value'])
+        if existresponse['hits']['total']['value'] > 0:
+            print(existresponse['hits']['total']['value'])
+            existing_employee = existresponse['hits']['hits'][0]['_source']
+            print(existing_employee)
+            
+            return JsonResponse({"message": "Employee already exists with the same details."}, status=400)
 
-    # Return the response body as a JSON response
-    return JsonResponse(resp_body, safe=False)
+        
+        employee_data = {
+            "FirstName": data.get("first_name"),
+            "LastName": data.get("last_name"),
+            "Designation": data.get("designation"),
+            "Salary": data.get("salary"),
+            "DateOfJoining": data.get("date_of_joining"),
+            "Address": data.get("address"),
+            "Gender": data.get("gender"),
+            "Age": data.get("age"),
+            "MaritalStatus": data.get("marital_status"),
+            "Interests": data.get("interests")
+        }
 
+        response = es.index(index="employee_db", body=employee_data)
+        
+        print(response)
+        response_data = str(response)
+        
+        return JsonResponse({"message": "Employee Created successfully", "data": response_data}, status=201)
+
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
